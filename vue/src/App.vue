2ed488235e1dea7a5d005294fff0216ca3a619a7
Nix Young <impactresonance+tech@gmail.com>
2023-09-26 21:28:19 -0400

    resolving merge conflicts

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -9,42 +9,13 @@
   </div>
 </template>
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a66e1114757bdbf1f47d44700d7cbea9bd3513b
 <script>
 export default {
   name: 'App'
 }
 </script>
 
-<<<<<<< HEAD
-
 <style>
-label {
-  margin-right: 0.5rem;
-}
-
-#login {
-  padding-top: 200px;
-  background-size: cover;
-  position: absolute;
-  top: 200px;
-  right: 0px;
-  bottom: 0px;
-  left: 0px;
-  background-image: url("assets/LoginBackground.jpg");
-}
-
-#login button:hover {
-  background-color: #4D565E;
-  color: white;
-}
-
-=======
-<style>
->>>>>>> 9a66e1114757bdbf1f47d44700d7cbea9bd3513b
 body {
   background: url("assets/LoginBackground.jpg") #fff;
   background-size: cover;
@@ -55,17 +26,6 @@ body {
 div {
  text-align: center;
 }
-<<<<<<< HEAD
-
-
-.form-input-group {
-  color: #4D565E;
-  text-align: center;
-  margin: 10px auto 30px;
-  border-radius: 15px;
-}
-=======
->>>>>>> 9a66e1114757bdbf1f47d44700d7cbea9bd3513b
 
 label {
   margin-right: 0.5rem;
