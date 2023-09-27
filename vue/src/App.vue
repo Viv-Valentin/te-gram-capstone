a3e8596ad6373a21a93d8a1a77f6b7da0b867dde
Nix Young <impactresonance+tech@gmail.com>
2023-09-26 20:42:13 -0400

    resolving merge errors

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -9,56 +9,13 @@
   </div>
 </template>
 
-<<<<<<< HEAD
-=======
 <script>
 export default {
   name: 'App'
 }
 </script>
->>>>>>> e434d22df3fb8543fa91da8658d28d7ef9b4ac39
 
 <style>
-.form-input-group, h1 {
-  margin-bottom: 1rem;
-  color: #4D565E;
-  font-family: serif;
-  text-align: center;
-  width: 300px;
-  margin: 10px auto 30px;
-  border-radius: 15px;
-}
-
-label {
-  margin-right: 0.5rem;
-}
-
-#login {
-  padding-top: 200px;
-  background-size: cover;
-  position: absolute;
-  top: 200px;
-  right: 0px;
-  bottom: 0px;
-  left: 0px;
-  background-image: url("assets/LoginBackground.jpg");
-}
-
-#login button:hover {
-  background-color: #4D565E;
-  color: white;
-}
-
-#login button {
-  width: 80px;
-  height: 30px;
-  border-radius: 8px;
-  border-style: solid;
-  border-width: thin;
-  font-weight: bold;
-   font-family: serif;
-}
-
 body {
   background: url("assets/LoginBackground.jpg") #fff;
   background-size: cover;
@@ -68,16 +25,6 @@ body {
 
 div {
  text-align: center;
-}
-<<<<<<< HEAD
-</style>
-=======
-
-.form-input-group {
-  color: #4D565E;
-  text-align: center;
-  margin: 10px auto 30px;
-  border-radius: 15px;
 }
 
 label {
@@ -98,5 +45,10 @@ button:hover {
   color: white;
 }
 
+.form-input-group {
+  color: #4D565E;
+  text-align: center;
+  margin: 10px auto 30px;
+  border-radius: 15px;
+}
 </style>
->>>>>>> e434d22df3fb8543fa91da8658d28d7ef9b4ac39
