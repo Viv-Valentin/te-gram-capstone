b126d2b2e9baa8577a6911ffdac72bc7cca8b1fe
Nix Young <impactresonance+tech@gmail.com>
2023-09-27 20:08:53 -0400

    updating post and feed methods

--- java/src/main/java/com/techelevator/dao/JdbcPostDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcPostDao.java
@@ -1,13 +1,10 @@
 package com.techelevator.dao;
 import com.techelevator.model.Post;
 import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.List;
 @Component
@@ -22,7 +19,7 @@ public class JdbcPostDao implements PostDao {
 
     @Override
     public boolean addPost(String username, String caption, String imageUrl) {
-        String sql = "INSERT INTO posts (username, caption, image_url) VALUES (?, ?, ?) RETURNING post_id";
+        String sql = "INSERT INTO posts (username, caption, image_url) VALUES (?, ?, ?)";
         try {
             jdbcTemplate.queryForObject(sql, int.class, username, caption, imageUrl);
         } catch (DataAccessException e) {
@@ -33,8 +30,13 @@ public class JdbcPostDao implements PostDao {
 
     @Override
     public List<Post> getFeed(String username) {
-        List<Post> myPost = new ArrayList<>();
-        return myPost;
+        List<Post> posts = new ArrayList<>();
+        String sql = "SELECT * FROM posts;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()) {
+            posts.add(mapRowToRow(results));
+        }
+        return posts;
     }
 
     @Override
--- vue/src/views/Post.vue
+++ vue/src/views/Post.vue
@@ -1,21 +1,11 @@
 <template>
     <div id="post">
-        <!-- <cloudinary-comp msg="Welcome to Your Vue.js App" /> -->
-        <table>
-            <tr v-for="post in posts" v-bind:key="post.id">
-                <td>{{ post.id }}</td>
-                <td>{{ post.caption }}</td>
-                <td>{{ post.imgUrl }}</td>
-            </tr>
-        </table>
         <add-post />
     </div>
 </template>
 
 <script>
 import AddPost from '../components/AddPost.vue';
-// import cloudinaryComp from '../components/CloudinaryComp.vue'
-import postService from '../services/PostService.js';
 
 export default {
     name: 'Post',
@@ -25,15 +15,7 @@ export default {
         }
     },
     components: {
-        // cloudinaryComp,
         AddPost
-    },
-    created() {
-        postService.addPost(this.post).then(
-            response => {
-                this.posts = response.data;
-            }
-        )
     }
 }
 </script>
